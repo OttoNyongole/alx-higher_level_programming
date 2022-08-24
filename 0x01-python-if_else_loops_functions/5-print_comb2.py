20c407c0f8d3cb81c880ba28361b81e0037a823c
OttoNyongole <nyongole.otto@gmail.com>
2022-08-24 12:37:51 +0300

    printing comb numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for n in range(0, 100):
-    if n == 99:
-        print("99", end='\n')
+for i in range(0, 100):
+    if i < 99:
+        print("{:02d}".format(i), end=', ')
     else:
-        print("{0:0=2d}, ".format(n), end='')
+        print("{:02d}".format(i), end='\n')
