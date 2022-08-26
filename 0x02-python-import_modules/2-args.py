15bb887dbf44bf4dcf5abd5a5ec4c68848d4b2c5
OttoNyongole <nyongole.otto@gmail.com>
2022-08-26 18:41:40 +0300

    changed methods

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-def main():
-    arg = len(sys.arg) - 1
-    if arg == 0:
-        print("0 arguments.")
-    elif arg == 1:
-        print("1 arguments.")
-        print("1: {}".format(sys.argv[1]))
-    else:
-        print("{:d} arguments:".format(argc))
-        for i in range(1, argc + 1):
-            print("{}:{}".format(i, sys.argv[i]))
-    main()
+    lst = sys.argv
+    lth = len(lst) - 1
+    arg = "argument"
+    print("{} argument{}{}".format(
+            lth,
+            "" if lth == 1 else "s",
+            "." if lth == 0 else ":"))
+    for i in range(1, len(lst)):
+        print("{}: {}".format(i, lst[i]))
