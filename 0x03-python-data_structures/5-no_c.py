9478d73940f2b95490e24c45dad3df9af2b2b855
Eng-Otto <nyongole.otto@gmail.com>
2022-08-29 22:34:30 +0300

    removed typos

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     if my_string:
         for char in list(my_string):
             if char == 'c' or char == 'C':
-                my_string.revove(char)
+                my_string.remove(char)
         return my_string
