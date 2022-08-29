f12970c4dd169d7ded2fef01ff4679c8c15c878d
Eng-Otto <nyongole.otto@gmail.com>
2022-08-29 22:41:45 +0300

    defined list of chars

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        for char in list(my_string):
-            if char == 'c' or char == 'C':
-                my_string.remove(char)
-        return my_string
+    list_chars = list(my_string)
+    for char in list_chars:
+        if char == 'c' or char == 'C':
+            list_chars.remove(char)
+    return("".join(list_chars))
