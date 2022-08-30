1567350e78295def73613740194861d10be3645e
Eng-Otto <nyongole.otto@gmail.com>
2022-08-30 13:25:18 +0300

    function that return tule with first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuplelength = len(sentence):
-    first_char = sentence[0] if tuplelength > 0 else "None"
-    tuple = tuplelength, first_char
-    return tuple
+    if len(sentence) == 0:
+        my_tuple = (0, None)
+    else:
+        my_tuple = (len(sentence), sentence[:1])
+    return(my_tuple)
