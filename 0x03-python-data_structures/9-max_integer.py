9866d765ac6d3419da122987bc67ee4e97d5e68b
Eng-Otto <nyongole.otto@gmail.com>
2022-08-30 13:52:24 +0300

    redefined sort method

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if leng(my_list) < 0:
+    if my_list == []:
         return None
-    else:
-        sortedlist = my_list.sort()
-        return sortedlist[-1]
+    my_list.sort()
+    return my_list[-1]
