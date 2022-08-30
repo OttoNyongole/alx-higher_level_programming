c83247b1065abe0d8e1655f03a2747ef00cc85bd
OttoNyongole <nyongole.otto@gmail.com>
2022-08-30 18:20:35 +0300

    clearing white spaces

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,14 +7,15 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *cursor = NULL, *start = NULL, *end = NULL;
+
 	if (*head == NULL)
 		return (1);
 	start = *head;
-	while (sart != end)
+	while (start != end)
 	{
 		cursor = start;
 		while (cursor->next && cursor->next != end)
-			cutsor = cutsor->next;
+			cursor = cursor->next;
 		end = cursor;
 		if (start->n != end->n)
 			return (0);
