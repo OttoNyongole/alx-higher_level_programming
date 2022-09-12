27aeda7e96fee3afc14b652462a6b2070bc27c84
OttoNyongole <nyongole.otto@gmail.com>
2022-09-12 18:19:31 +0300

    chenged elements definitation

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list[], x = 0):
-    try:
-        for i in range(x):
-            print("{:d}".format(my_list[i]), end="")
-            print()
-            return x
-        except IndexError
-            print()
-            return i
+    elements = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            elements += 1
+        except:
+            pass
+        print()
+        return elements
