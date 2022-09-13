3cb34615cf3ece0b9f753b65caddb0283adeed7d
Eng-Otto <nyongole.otto@gmail.com>
2022-09-13 22:42:38 +0300

    created size of the square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    ""Defining a Square"""
+    def __init__(self, size):
+        """Initializing attribute to define a square"""
+        self.__size = size
