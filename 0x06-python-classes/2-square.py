2db6b566064f36380d88b1911094ec397f661945
Eng-Otto <nyongole.otto@gmail.com>
2022-09-13 23:04:38 +0300

    added comments

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,9 @@
 
 
 class Square:
+    """Define a Square"""
     def __init__(self, size=0):
+        """Validate size value and Initialize Square attribute"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
