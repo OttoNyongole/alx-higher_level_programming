6066ef7d8f630e8baf2d76cbf7cfb22999defd22
Eng-Otto <nyongole.otto@gmail.com>
2022-09-14 20:03:51 +0300

    function to print the first element of a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    elements = 0
+    counter = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            elements += 1
+            counter += 1
         except (ValueError, TypeError):
             pass
     print()
-    return elements
+    return counter
