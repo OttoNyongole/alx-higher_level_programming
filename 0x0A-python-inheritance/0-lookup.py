42719ef5c10f835e767fee20d01904f186cd865a
Eng-Otto <nyongole.otto@gmail.com>
2022-09-26 20:18:54 +0300

    implementing dir() function of and object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that returns attributes and methods of an object"""
+
+
+
+def lookup(obj):
+    """returns the available attribute nd methods as a return value"""
+    return dir(obj)
