aea8c97f869097d7faf2c2a2ca8e7ea961ecab0c
Eng-Otto <nyongole.otto@gmail.com>
2022-09-26 21:03:26 +0300

    correction on python doc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""function that returns attributes and
-   methods of an object
+"""
+function that returns attributes and methods of an object
 """
 def lookup(obj):
     """returns the available attribute nd methods as a return value"""
