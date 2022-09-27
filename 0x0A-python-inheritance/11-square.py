de8b3314a49e27f24eabdef24814b3c7d1ebfc75
Eng-Otto <nyongole.otto@gmail.com>
2022-09-27 19:13:43 +0300

    square class inherits from parent square class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Rep square."""
+
+    def __init__(self, size):
+        """instatiate
+        Args:
+            size (int): size
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
