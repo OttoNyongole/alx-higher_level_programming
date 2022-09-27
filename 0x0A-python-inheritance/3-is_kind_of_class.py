a65e4eac80f96d9e1e889afbf8a5dee79e234de6
Eng-Otto <nyongole.otto@gmail.com>
2022-09-27 15:20:42 +0300

    pystyle correction

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""The module"""
-
-
+"""My module"""
 def is_kind_of_class(obj, a_class):
-    """checks if is an inheritance or intance of a class that inherited"""
-    Agrs:
-        obj = the object to check
-        a_class = the class to match the type with.
+    """checks if it is exact the same or inherited
+    Args:
+         obj(any) = the object to check
+         a_class(type) = the class to match the type with
     Return:
-          True if the object is exactly an instance
-          otherwise False
+           True if the object is exactly an instance
+           otherwise False
     """
     if isinstance(obj, a_class):
         return True
-    return False
+    return 
