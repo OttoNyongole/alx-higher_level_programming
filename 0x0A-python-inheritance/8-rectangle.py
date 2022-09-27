6d6f6fba7c1c6604ab0e58725383e1d040d512b0
Eng-Otto <nyongole.otto@gmail.com>
2022-09-27 18:57:36 +0300

    correction indentation error

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""defines a class rectangle that inherits from class BaseGeometry"""
+"""Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle(Basegeometry):
-    """inheriting from BaseGeometry"""
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
 
-    def__init__(self, width, height):
-        """
-        initialize new Rectangle.
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
         Args:
-            width (int): the width of the rectangle.
-            Height (int): the height of the rectangle.
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
         """
-
         self.integer_validator("width", width)
-        self.__wisth = width
+        self.__width = width
         self.integer_validator("height", height)
         self.__height = height
