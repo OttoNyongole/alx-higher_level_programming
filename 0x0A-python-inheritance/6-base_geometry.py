55d4c132efa6aebb91c3b6d6d47ed1f0870b9bd2
Eng-Otto <nyongole.otto@gmail.com>
2022-09-27 17:45:13 +0300

    raising exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeomentry:
+    """Rep base geometry."""
+
+    def area(self):
+        """NULL statements"""
+        raise Exception("area() is niot implemented")
