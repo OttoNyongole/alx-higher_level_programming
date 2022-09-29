e2c79416cb065b89c0696462fdc464562bc2b040
Eng-Otto <nyongole.otto@gmail.com>
2022-09-29 19:29:56 +0300

    imported os path

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,17 @@
 """module that add all system args to a list of json file"""
 
 import sys
+import os.path
 
 
 save_to json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-try:
-    items = load_from_json_file(filename)
-except FileNotFoundError:
-    items = []
-items.extend(sys.argv[1:])
-save_to_json_file(items, filename)
+
+if os.path.isfile(filename):
+    json_file = load_from_json_file(filename)
+    json_list = json_file + sys.argv[1:]
+    save_to_json_file(json_list, filename)
+else:
+    save_to_json_file(sys.argv[1:], filename)
