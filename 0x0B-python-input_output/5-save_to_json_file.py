6e64ce8305881a0ef660c133f34633f78f1e4647
Eng-Otto <nyongole.otto@gmail.com>
2022-09-29 19:03:19 +0300

    correction write method in last line

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,4 +10,4 @@ def save_to_json_file(my_obj, filename):
         filename(file pointer): pointer to json file
     """
     with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+        file.write(json.dumps(my_obj))
