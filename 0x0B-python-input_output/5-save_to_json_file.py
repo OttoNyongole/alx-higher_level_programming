6d6cbff31766b541aa44b0402a7f0f010907d752
Eng-Otto <nyongole.otto@gmail.com>
2022-09-29 18:58:44 +0300

    converting an object text file to json replesentation

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to write an object to text file using json replesentation"""
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """
+    Args:
+        my_obj(dict): object to be converted to a JSON object
+        filename(file pointer): pointer to json file
+    """
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
