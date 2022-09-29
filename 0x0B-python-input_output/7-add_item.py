a73b64353bb33bf3ec459b8b1c568eb80185ae71
Eng-Otto <nyongole.otto@gmail.com>
2022-09-29 19:38:48 +0300

    documentation of the module

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,6 +12,11 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
 if os.path.isfile(filename):
+    """
+    Args:
+        filename(str): name of the object file loaded from argv
+        also checks the file if it does exist if not create one
+    """
     json_file = load_from_json_file(filename)
     json_list = json_file + sys.argv[1:]
     save_to_json_file(json_list, filename)
