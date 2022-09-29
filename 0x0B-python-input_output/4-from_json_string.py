f45d0cb4d12c8e0c6943a2fe958118adddac344f
Eng-Otto <nyongole.otto@gmail.com>
2022-09-29 18:50:22 +0300

    converting json string to python dictionary

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module to return object replesented frm JSON string"""
+
+import json
+
+def from_json_string(my_str):
+    """
+    loads(): Method converts JSON str to python dictionary
+    Args:
+        my_str(string): json string to be converted to python dictionary
+    """
+    return json.loads(my_str)
