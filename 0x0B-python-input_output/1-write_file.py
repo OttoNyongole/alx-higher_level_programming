4280ef17025e03385751969a9889b8c2b72a8419
Eng-Otto <nyongole.otto@gmail.com>
2022-09-29 16:03:00 +0300

    correction wrote mode

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,10 +4,13 @@ Module for returning number of lines in text file
 """
 
 
-def number_of_lines(filename=""):
-    """ returns number of lines in file """
-    counter = 0
-    with open(filename, encoding="utf-8") as a_file:
-        for line in a_file:
-            counter = counter + 1
-    return counter
+def write_file(filename="", text=""):
+    """uses open() and write() to write string to a text file.
+    Return:
+        no of characters read
+    Args:
+        filename: text file to write into
+        text (str): string to write to text file
+    """
+    with open(filename, 'w', encoding='utf8') as file:
+        return file.write(text)
