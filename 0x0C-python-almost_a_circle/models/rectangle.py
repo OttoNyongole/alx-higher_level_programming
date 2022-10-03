7afd0e1fd2a5f080c7a49c75fdfee3e533a78cdc
Eng-Otto <nyongole.otto@gmail.com>
2022-10-03 19:18:37 +0300

    pushed main test file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Module for class Rectangle"""
-from models.base import base
+from models.base import Base
 
 
 
 class Rectangle(Base):
     """class Rectangle that inherits from Base"""
-    def __init__(self, width, height, x=0, y=0, id=none):
+    def __init__(self, width, height, x=0, y=0, id=None):
         """Construct a class Rectangle"""
 
         super().__init__(id)
@@ -42,7 +42,7 @@ class Rectangle(Base):
                 raise TypeError("height must be an integer")
 
             if value <= 0:
-                raise ValueError(height must > 0)
+                raise ValueError("height must be > 0")
 
             self.__height = value
 
