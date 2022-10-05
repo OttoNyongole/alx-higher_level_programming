17c55c90e86e9fd17303c52d5f7c70b6aa696a20
Eng-Otto <nyongole.otto@gmail.com>
2022-10-05 19:41:41 +0300

    correction pycodestyle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-"""Module for class Rectangle"""
+"""
+This module contains the Class Rectangle.
+"""
 from models.base import Base
+
+
 class Rectangle(Base):
-    """class Rectangle that inherits from Base"""
+    """
+    This Class Rectangle inherit from Base.
+    """
     def area(self):
-            """Returns the area of the rectangle"""
-            return self.width * self.height
+        """area method
+        Return:
+            area the rectangle
+        """
+        return self.width * self.height
 
     def display(self):
-        """Display character # in stdout"""
+        """display method: print rectangle with symbol
+        Return:
+            none
+        """
         _display = ''
 
         _display += "\n" * self.y
@@ -19,25 +31,22 @@ class Rectangle(Base):
         print(_display)
 
     def update(self, *args, **kwargs):
-        """
-        update method
+        """update method
         Args:
-            *args:
-                pointer to an array of data
+            @*args: pointer to a array data
+            @**kwargs: double pointer to a dictionary: key/value
         """
-        if args and len(args):
+        if args and len(args) > 0:
             for idx, arg in enumerate(args):
                 if idx == 0:
                     super().__init__(arg)
                 if idx == 1:
-                    self.id = arg
-                if idx == 2:
                     self.width = arg
-                if idx == 3:
+                if idx == 2:
                     self.height = arg
-                if idx == 4:
+                if idx == 3:
                     self.x = arg
-                if idx == 5:
+                if idx == 4:
                     self.y = arg
         elif kwargs and len(kwargs) > 0:
             for key, value in kwargs.items():
@@ -51,6 +60,7 @@ class Rectangle(Base):
                     self.x = value
                 if key == 'y':
                     self.y = value
+
     def to_dictionary(self):
         """to_dictionary method
         Return:
@@ -65,63 +75,90 @@ class Rectangle(Base):
         attrs_list = ["id", "width", "height", "x", "y"]
         return {key: getattr(self, key) for key in attrs_list}
 
-
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Construct a class Rectangle"""
+        """
+        init method
+        Args:
+            @width: width rectangle.
+            @height: height rectangle.
+            @x: x position
+            @y: y position
+        """
         super().__init__(id)
+
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        @property
-        def width(self):
-            """get the value of width"""
-            return self.__width
-        @width.setter
-        def width(self, value):
-            """sets the value to the width"""
-            if type(value) != int:
-                raise TypeError("width must be an integer")
-            if value <= 0:
-                raise ValueError("width must be > 0")
-            self.__width = value
-        @property
-        def height(self):
-            """get the value of height"""
-            return self.__height
-        @height.setter
-        def height(self, value):
-            """sets the value to the height"""
-            if type(value) != int:
-                raise TypeError("height must be an integer")
-            if value <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = value
-        @property
-        def x(self):
-            """get the value of x """
-            return self.__x
-        @x.setter
-        def x(self, value):
-            """sets the value to x """
-            
-            if type(value) != int:
-                raise TypeError("x must be an integer")
-            if value < 0:
-                raise ValueError("x must be >= 0")
-            self.__x = value
-        @property
-        def y(self):
-            """get the value of y"""
-            return self.__y
-        @y.setter
-        def y(self, value):
-            """sets the value to y"""
-            if type(value) != int:
-                raise TypeError("y must be an integer")
-            if value < 0:
-                raise ValueError("y must be >= 0")
-            self.__y = value
-        def __str__(self):
-            """Override str method from Base"""
-return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.x, self.y, self.width, self.height)
+
+    @property
+    def width(self):
+        """getter width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter width
+        Attributes:
+            @__width: rectangle width
+        Args:
+            @value: rectangle width value
+        """
+        self.integer_validator("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter height
+        Attributes:
+            @__height: rectangle height
+        Args:
+            @value: rectangle height value
+        """
+        self.integer_validator("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        """getter x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter x
+        Attributes:
+            @__x: x position
+        Args:
+            @value: x position value
+        """
+        self.integer_validator("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """getter y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter y
+        Attributes:
+            @__y: y position
+        Args:
+            @value: y position value
+        """
+        self.integer_validator("y", value)
+        self.__y = value
+
+    def __str__(self):
+        """magic method __str__
+        Return:
+            [Rectangle] (<id>) <x>/<y> - <width>/<height>
+        """
+        _print = "[Rectangle] ({}) {}/{} - {}/{}"
+        return _print.format(self.id, self.x, self.y, self.width, self.height)
