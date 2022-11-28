72668ae3571a26faa5d5035d3e0dd5cde6728a08
OttoNyongole <nyongole.otto@gmail.com>
2022-11-28 17:51:19 +0300

    printing string in independent lines using console.log()

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 
 //stript to print 3 strings in 3 line
-console.log('C is fun', '\npython is cool', '\nJavascrit is amazing');
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
