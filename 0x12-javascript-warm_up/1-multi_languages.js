3f9c62277b6746e2396f4c66c90eba034fa0e2c6
OttoNyongole <nyongole.otto@gmail.com>
2022-11-30 15:30:09 +0300

    removed third line of coment

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-
-//stript to print 3 strings in 3 line
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
