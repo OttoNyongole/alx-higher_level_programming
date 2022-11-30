12c9b3f9f5dde6777c45ab762f7d69fb1a7748f5
OttoNyongole <nyongole.otto@gmail.com>
2022-11-30 09:41:28 +0300

    script that print 3 lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// JS Script for array of data
+
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
