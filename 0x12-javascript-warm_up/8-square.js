b406919eafc54fbb99c8e2d411fcb91cdc975669
OttoNyongole <nyongole.otto@gmail.com>
2022-11-30 10:00:23 +0300

    script to print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+//script to print a square
+const x = process.argv[2];
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+}
