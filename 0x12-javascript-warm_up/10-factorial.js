e879dbd4f80090c2b1a2c313e4909117c0e41539
OttoNyongole <nyongole.otto@gmail.com>
2022-11-30 13:10:43 +0300

    removed empt lines in script

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // JS factorial of a number
-
 function factorial (n) {
   if (isNaN(n) || n === 0) {
     return (1);
@@ -8,5 +7,4 @@ function factorial (n) {
    return n * factorial(n - 1); 
  }
 }
-
 console.log(factorial(parseInt(process.argv[2])));
