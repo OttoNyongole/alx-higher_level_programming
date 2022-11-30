17830d775cc2301d14ed67a6ddc5805991a0af1a
OttoNyongole <nyongole.otto@gmail.com>
2022-11-30 13:32:40 +0300

    factorial of a number

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-// JS factorial of a number
 function factorial (n) {
   if (isNaN(n) || n === 0) {
-    return (1);
- } else {
-   return n * factorial(n - 1); 
- }
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
+
 console.log(factorial(parseInt(process.argv[2])));
