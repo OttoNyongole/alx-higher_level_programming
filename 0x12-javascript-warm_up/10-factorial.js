08507636a00d1fbd9b200206c67527abe8514fba
OttoNyongole <nyongole.otto@gmail.com>
2022-11-30 11:26:43 +0300

    script to print the factorial of a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// JS factorial of a number
+
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+ } else {
+   return n * factorial(n - 1); 
+ }
+}
+
+console.log(factorial(parseInt(process.argv[2])));
