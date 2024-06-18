d9c4f8b5487805c6776e0308db72d29a57ea1422
OttoNyongole <104821618+OttoNyongole@users.noreply.github.com>
2024-06-18 20:52:04 +0300

    Update 2-arguments.js spacing added

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Script that prints massage depending of the number of arguments passed
-const count = process.argv.length -2; // this is because 1 and 2 are node and file path to be excuted respectively
+const count = process.argv.length - 2; // this is because 1 and 2 are node and file path to be excuted respectively
 if (count === 0) {
   console.log('No argument');
 } else if (count === 1) {
