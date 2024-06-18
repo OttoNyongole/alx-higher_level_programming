6240f3ce1acfa4683f219f5873cd19eb52b2f3d7
OttoNyongole <104821618+OttoNyongole@users.noreply.github.com>
2024-06-18 20:50:17 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-
 // Script that prints massage depending of the number of arguments passed
-
-const count = process.argv.length;
-
-if (count === 2) {
+const count = process.argv.length -2; // this is because 1 and 2 are node and file path to be excuted respectively
+if (count === 0) {
   console.log('No argument');
-} else if (count === 3) {
+} else if (count === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
