3684ba06ad130b09d88ce34899c39f9cb7089f23
OttoNyongole <104821618+OttoNyongole@users.noreply.github.com>
2024-06-18 20:39:20 +0300

    Update 0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-// constant variable called myVar with the value "Javascript is amazing"
-const myVar = “JavaScript is amazing”;
-// printing the value of the variable myVar using console.log()
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
